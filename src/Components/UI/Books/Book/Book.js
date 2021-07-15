d1868546e45651388233de6b041f420e794afda3
ZQYM6RI <dalviavishkar@johndeere.com>
2021-07-15 18:54:23 +0530

    fix : book component modified

--- src/Components/UI/Books/Book/Book.js
+++ src/Components/UI/Books/Book/Book.js
@@ -11,7 +11,7 @@ const Book = ({ book }) => {
                 <h2>{title}</h2>
                 {subtitle && <h3>{subtitle}</h3>}
             </div>
-            {textSnippet && <p>{textSnippet}</p>}
+            {textSnippet && <p>{textSnippet}</p>}x
         </div>
     )
 }
