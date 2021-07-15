6a76869350f0a8f0c4aaa206506422d283ce4b8d
effzedwhy <fatemazakir@gmail.com>
2021-07-15 16:21:51 +0100

    changed UI & fixed div

--- src/Components/UI/Books/Book/Book.js
+++ src/Components/UI/Books/Book/Book.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from 'react'
 import classes from './Book.module.css'
 
 const Book = ({ book }) => {
-    const title = book.volumeInfo.title;
-    const subtitle = book.volumeInfo?.subtitle;
-    const textSnippet = book.searchInfo?.textSnippet;
-    return (
-        <div className={classes.bookLayout}>
-            <div className={classes.titleDiv}>
-                <h2>{title}</h2>
-                {subtitle && <h3>{subtitle}</h3>}
-            </div>
-            {textSnippet && <p>{textSnippet}</p>}x
-        </div>
-    )
+  const title = book.volumeInfo.title
+  const subtitle = book.volumeInfo?.subtitle
+  const textSnippet = book.searchInfo?.textSnippet
+  return (
+    <div className={classes.bookLayout}>
+      <div className={classes.titleDiv}>
+        <h2>{title}</h2>
+      </div>
+      {subtitle && <h3>{subtitle}</h3>}
+      {textSnippet && <p>{textSnippet}</p>}x
+    </div>
+  )
 }
 
 export default Book
